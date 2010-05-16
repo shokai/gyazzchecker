972e3c42a9c53876de48a5209d1b973d4bd80045
Sho Hashimoto <hashimoto@shokai.org>
2010-05-17 04:44:20 +0900

    remove '&' when post im-kayac

--- lib/im-kayac.rb
+++ lib/im-kayac.rb
@@ -4,6 +4,7 @@ require 'uri'
 module ImKayac
   def ImKayac.send(user, message)
     puts uri = URI.parse("http://im.kayac.com/api/post/#{user}")
+    message.gsub!(/&/, '_')
     Net::HTTP.start(uri.host, uri.port) {|http|
       response = http.post(uri.path, "message=#{URI.encode(message)}")
       puts response.body
